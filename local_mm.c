2e052340b30fb497d4caef9eb28e72f662dcf02d
Paul Kippes <kippesp@gmail.com>
2011-10-13 01:31:51 -0400

    changes so far

--- local_mm.c
+++ local_mm.c
@@ -8,6 +8,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MB *1024*1024
+#define KB *1024
+
+/* Intel Xeon X5650 CPU Gulftown (sixcore) */
+/* L1 and L2 cache are per core */
+#define L1_DATA_CACHE_SIZE (32 KB)
+#define L2_CACHE_SIZE (256 KB)
+#define L3_CACHE_SIZE (12 MB)
+
+#define TLB0_PAGE_SIZE (2 MB)
+#define TLB0_PAGE_SIZE_MAYBE (4 MB)
+#define TLB0_PAGE_ENTRIES 32
+#define L2_TLB_PAGE_ENTRIES 512
+
+#define TLB_PAGE_SIZE (4 KB)
+#define TLB_PAGE_ENTRIES 64
+#define L2_TLB_PAGE_SIZE (4 KB)
+
 #if defined(USE_TLB)
 # include <string.h>
 #endif
@@ -25,8 +43,8 @@
 #endif
 
 #define MIN(a, b)   ((a < b) ? a : b)
+#define MAX(a, b)   ((a > b) ? a : b)
 
-#ifdef FOR_DEBUG
 static void print_matrix(int rows, int cols, const double *mat) {
 
   int r, c;
@@ -41,7 +59,6 @@ static void print_matrix(int rows, int cols, const double *mat) {
     fprintf(stderr, "\n");
   } /* r */
 }
-#endif
 
 /**
  *
@@ -80,6 +97,60 @@ void local_mm(const int m, const int n, const int k, const double alpha,
   assert(ldc >= m);
 
 #ifdef USE_BLOCKING
+
+  int i, j;
+  double* At; /* A transposed, aligned to L2 TLB page */
+
+  /* Work on a portion of A */
+  const int num_a_blocks = MAX(1, sizeof(double)*m*k / L2_CACHE_SIZE / 2);
+  int a_block_idx;
+  const int num_at_rows = m / num_a_blocks;
+
+  assert(0 == posix_memalign((void**)&At, L2_TLB_PAGE_SIZE, L2_CACHE_SIZE));
+
+  for (a_block_idx = 0; a_block_idx < num_a_blocks; a_block_idx++)
+  {
+    int a_row;
+    int a_col;
+
+    /* Transpose A and preload L2 cache */
+    for (a_row = 0; a_row < m / num_a_blocks; a_row++)
+    {
+      for (a_col = 0; a_col < k; a_col++)
+      {
+        At[a_col + a_row*num_at_rows] = A[a_row + a_col*m];
+        assert(a_col + a_row*num_at_rows < m*k);
+      }
+    }
+
+  /* TODO: Iterate over transposed A rows/cols */
+  /* TODO: Are we assuming m==n? */
+
+  /* Iterate over the columns of C */
+  for (col = 0; col < n; col++) {
+
+    /* Iterate over the rows of C */
+    for (row = 0; row < m; row++) {
+
+      int k_iter;
+      double dotprod = 0.0; /* Accumulates the sum of the dot-product */
+
+      /* Iterate over column of A, row of B */
+      for (k_iter = 0; k_iter < k; k_iter++) {
+        int a_index, b_index;
+        a_index = (row * lda) + k_iter; /* Compute index of A element */
+        b_index = (col * ldb) + k_iter; /* Compute index of B element */
+        dotprod += b2[a_index] * B[b_index]; /* Compute product of A and B */
+      } /* k_iter */
+
+      int c_index = (col * ldc) + row;
+      C[c_index] = (alpha * dotprod) + (beta * C[c_index]);
+    } /* row */
+  } /* col */
+
+
+
+  }
 
 
 
