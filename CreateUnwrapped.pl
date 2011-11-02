26d79653b9d8eab7b8bf38c4cead6d47bd258d9a
silenthunter <silenthunter@gmail.com>
2011-11-02 17:44:03 -0400

    Tries to convert to floats before SSE. Faster, but accuracy is lost. Probably not in the spirit of tha project

--- CreateUnwrapped.pl
+++ CreateUnwrapped.pl
@@ -16,10 +16,11 @@ open(FILEOUT, ">matmult_blocked_copy.c");
 
 my $unrolled = "";
 
+$unrolled .= "if(K % BLOCK_SIZE != 0) return;\n";
 $unrolled .= "int i, j, k;\nint bs = BLOCK_SIZE;\n";
-$unrolled .= "__m128d a, b, a1, b1, prod0, tempDot0, prod1, tempDot1;\n";
-$unrolled .= "__declspec(align(16)) double prodResult[2];\n";
-$unrolled .= "double dotProd;\n\n";
+$unrolled .= "__m128 a, b, a1, b1, prod0, tempDot0, prod1, tempDot1;\n";
+$unrolled .= "__declspec(align(16)) float prodResult[4];\n";
+$unrolled .= "float dotProd;\n\n";
 
 $unrolled .= "for(i = 0; i < $N; i++)\n{\n";
 #for(my $i = 0; $i < 48; $i++)
@@ -27,10 +28,10 @@ $unrolled .= "for(i = 0; i < $N; i++)\n{\n";
 	$unrolled .= "for(j = 0; j < $N; j++)\n{\n";
 	#for(my $j = 0; $j < $N; $j++)
 	#{
-		$unrolled .= "dotProd = 0.0;\nprod0 = _mm_set1_pd(0);\nprod1 = _mm_set1_pd(0);\n\n";
+		$unrolled .= "dotProd = 0.0;\nprod0 = _mm_set1_ps(0);\nprod1 = _mm_set1_ps(0);\n\n";
 		
 		my $alt = 1;
-		for(my $k = 0; $k < $N; $k+=2)
+		for(my $k = 0; $k < $N; $k+=4)
 		{
 			my $preA = "a";
 			my $preB = "b";
@@ -39,11 +40,11 @@ $unrolled .= "for(i = 0; i < $N; i++)\n{\n";
 				$preA .= $alt;
 				$preB .= $alt;
 			}
-			$unrolled .= "$preA = _mm_load_pd(&A[i * K + " .$k."]);\n";
-			$unrolled .= "$preB = _mm_load_pd(&B[j * K + ".$k."]);\n";
+			$unrolled .= "$preA = _mm_load_ps(&A[i * K + " .$k."]);\n";
+			$unrolled .= "$preB = _mm_load_ps(&B[j * K + ".$k."]);\n";
 			
-			$unrolled .= "tempDot$alt = _mm_mul_pd($preA, $preB);\n";
-			$unrolled .= "prod$alt = _mm_add_pd(prod$alt, tempDot$alt);\n";
+			$unrolled .= "tempDot$alt = _mm_mul_ps($preA, $preB);\n";
+			$unrolled .= "prod$alt = _mm_add_ps(prod$alt, tempDot$alt);\n";
 			$alt += 1;
 			if($alt > 1)
 			{
@@ -51,11 +52,11 @@ $unrolled .= "for(i = 0; i < $N; i++)\n{\n";
 			}
 		}
 		
-		$unrolled .= "prod0 = _mm_add_pd(prod0, prod1);\n";
-		$unrolled .= "_mm_store_pd(prodResult, prod0);\n";
-		$unrolled .= "dotProd += prodResult[0] + prodResult[1];\n";
+		$unrolled .= "prod0 = _mm_add_ps(prod0, prod1);\n";
+		$unrolled .= "_mm_store_ps(prodResult, prod0);\n";
+		$unrolled .= "dotProd += prodResult[0] + prodResult[1] + prodResult[2] + prodResult[3];\n";
 		#my $cIndex = $j * $N;
-		$unrolled .= "C[j * lda + i] = dotProd + C[j * lda + i];\n\n";
+		$unrolled .= "C[j * lda + i] = (double)dotProd + C[j * lda + i];\n\n";
 	#}
 	$unrolled .= "}\n";
 #}
