b2788999ab9a7b505e4fa9dbb9fa9d24b4b3bd22
silenthunter <silenthunter@gmail.com>
2011-11-08 10:33:20 -0500

    Trying to do register blocking. Performance is much worse

--- CreateUnwrapped.pl
+++ CreateUnwrapped.pl
@@ -21,6 +21,9 @@ $unrolled .= "__m128d a, b, a1, b1, b2, b12, prod, tempDot, prod1, tempDot1;\n";
 $unrolled .= "__declspec(align(16)) double prodResult[2];\n";
 $unrolled .= "double dotProd;\n\n";
 
+$unrolled .= "__declspec(align(16)) double lB[bs];\n";
+$unrolled .= "__declspec(align(16)) double lB2[bs];\n";
+
 $unrolled .= "for(i = 0; i < $N; i++)\n{\n";
 #for(my $i = 0; $i < 48; $i++)
 #{
@@ -28,30 +31,22 @@ $unrolled .= "for(i = 0; i < $N; i++)\n{\n";
 	#for(my $j = 0; $j < $N; $j++)
 	#{
 		$unrolled .= "dotProd = 0.0;\nprod = _mm_set1_pd(0);\nprod1 = _mm_set1_pd(0);\n\n";
+		$unrolled .= "for(k = 0; k < bs; k++)\n{lB[k] = B[j * K + k];\nlB2[k] = B[(j + 1) * K + k];\n}\n";
 		
-		my $alt = 1;
+		#my $alt = 1;
 		for(my $k = 0; $k < $N; $k+=2)
 		{
 			my $preA = "a";
 			my $preB = "b";
-			if($alt > 0)
-			{
-				$preA .= $alt;
-				$preB .= $alt;
-			}
 			$unrolled .= "$preA = _mm_load_pd(&A[i * K + " .$k."]);\n";
-			$unrolled .= "$preB = _mm_load_pd(&B[j * K + ".$k."]);\n";
-			$unrolled .= $preB."2 = _mm_load_pd(&B[(j + 1) * K + ".$k."]);\n";
-			
+			$unrolled .= "$preB = _mm_load_pd(&lB[".$k."]);\n";
+			$unrolled .= $preB."2 = _mm_load_pd(&lB2[".$k."]);\n";
+		
 			$unrolled .= "tempDot = _mm_mul_pd($preA, $preB);\n";
 			$unrolled .= "prod = _mm_add_pd(prod, tempDot);\n";
 			$unrolled .= "tempDot1 = _mm_mul_pd($preA, ".$preB."2);\n";
-			$unrolled .= "prod1 = _mm_add_pd(prod, tempDot1);\n";
-			$alt += 1;
-			if($alt > 1)
-			{
-				$alt = 0;
-			}
+			$unrolled .= "prod1 = _mm_add_pd(prod1, tempDot1);\n";
+
 		}
 		
 		$unrolled .= "_mm_store_pd(prodResult, prod);\n";
